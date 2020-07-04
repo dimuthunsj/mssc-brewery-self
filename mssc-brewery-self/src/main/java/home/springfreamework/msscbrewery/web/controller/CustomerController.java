1a71d1cb33732c0f445cbad5fa935e42bed9a89c
dimuthunsj <dimuthunsj@gmail.com>
2020-07-04 21:36:20 +0530

    implement missing functionality

--- mssc-brewery-self/src/main/java/home/springfreamework/msscbrewery/web/controller/CustomerController.java
+++ mssc-brewery-self/src/main/java/home/springfreamework/msscbrewery/web/controller/CustomerController.java
@@ -4,12 +4,10 @@ import home.springfreamework.msscbrewery.web.model.BeerDto;
 import home.springfreamework.msscbrewery.web.model.CustomerDto;
 import home.springfreamework.msscbrewery.web.service.CustomerService;
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,5 +24,28 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getBeer(@PathVariable("customerId") UUID customerId){
         return new ResponseEntity<>(service.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> handlePost(@RequestBody CustomerDto dto){
+        CustomerDto customerDto = service.saveNewCustomer(dto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/cusomer/" + customerDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{customerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto dto){
+
+        service.updateCustomer(customerId, dto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("customerId") UUID customerId){
+        service.deleteById(customerId);
     }
 }
--- mssc-brewery-self/src/main/java/home/springfreamework/msscbrewery/web/service/Impl/CustomerServiceImpl.java
+++ mssc-brewery-self/src/main/java/home/springfreamework/msscbrewery/web/service/Impl/CustomerServiceImpl.java
@@ -2,12 +2,15 @@ package home.springfreamework.msscbrewery.web.service.Impl;
 
 import home.springfreamework.msscbrewery.web.model.CustomerDto;
 import home.springfreamework.msscbrewery.web.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto
@@ -15,5 +18,21 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Dimuthu")
                 .build();
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto dto) {
+        return CustomerDto
+                .builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+
     }
 }
